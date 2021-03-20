44297721349fc3b9eb47a81788bbc9afdd85efab
Feiyang1 <plane1113@gmail.com>
2021-03-19 22:25:54 -0700

    set up plugins

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,10 @@ import resolveModule from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import scss from 'rollup-plugin-scss'
+import copy from 'rollup-plugin-copy';
+import gzipPlugin from 'rollup-plugin-gzip';
+import visualizer from 'rollup-plugin-visualizer';
+import sizes from 'rollup-plugin-sizes';
 
 const plugins = [
   typescript({
@@ -20,14 +24,24 @@ const plugins = [
     mangle: { toplevel: true },
     compress: false
   }),
-  scss()
+  scss(),
+  copy({
+    targets: [{ src: 'index.html', dest: 'dist' }]
+  }),
+  gzipPlugin(),
+  visualizer({
+      template: 'treemap',
+      sourcemap: true,
+      gzipSize: true
+  }),
+  sizes()
 ];
 
 export default [
   {
     input: 'src/main.ts',
     output: [
-      { file: 'dist/bundle.js', format: 'iife' }
+      { file: 'dist/bundle.js', format: 'iife', sourcemap: true }
     ],
     plugins
   },
